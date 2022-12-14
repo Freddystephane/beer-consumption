441a9a6d63d478c2d1cd7baa2b1593080a6d44b4
Freddystephane <freddystephane000@gmail.com>
2022-12-14 12:02:05 +0000

    test4

--- domain/preprocessing.py
+++ domain/preprocessing.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import numpy as np
+import numpy as np 
+import matplotlib
 def  bewery_analyse(data):
     hightest=pd.DataFrame(data.groupby(['brewery_name']).beer_abv.nunique().sort_values(ascending=False).iloc[0:1])
     return hightest
